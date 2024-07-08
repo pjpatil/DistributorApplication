d72acc753774d842c59360645362e74d89071e1c
Pankaj j Patil <pp892810@gmail.com>
2024-07-08 11:05:47 +0530

    vendor added completed

--- eclipse-workspace/DistributorApplication/src/main/webapp/JS/validate.js
+++ eclipse-workspace/DistributorApplication/src/main/webapp/JS/validate.js
@@ -1,3 +1,20 @@
-/**
- * 
- */
+function validateVendor() {
+	var vendName = document.getElementById("txtVend").value;
+	let regExp = /^[a-zA-Z]+$/;
+	let result = regExp.test(vendName);
+	if (result == false) {
+		let span = document.getElementById("msg");
+		msg.innerHTML = "Invalid Vendor name";
+		let button = document.getElementById("btn");
+		button.disabled = true;
+		return false;
+	}
+	else {
+		let span = document.getElementById("msg");
+		msg.innerHTML = "";
+		let button = document.getElementById("btn");
+		button.disabled = false;
+		return true;
+	}
+
+}
